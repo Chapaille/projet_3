a7354426e8c5640ca9217b279961e8f28e3e63e9
Mark Webber <m.webber991@gmail.com>
2022-02-15 16:35:33 +0100

    Init

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    <script src="https://kit.fontawesome.com/c5d9feed49.js" crossorigin="anonymous"></script>
+
+    </head> 
+    <body>
+        <?php include "./header.html"?>
+
+
+
+        <?php include "./footer.html"?>
+    </body>
+
+
+</html>
